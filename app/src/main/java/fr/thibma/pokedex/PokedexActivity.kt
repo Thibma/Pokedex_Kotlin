50722e10947ee1c0c8b0d9bcefeef179dbfc7451
Thibma <balsamothibault@hotmail.fr>
2021-09-14 16:20:37 +0200

    Initial commit

---
+++ app/src/main/java/fr/thibma/pokedex/PokedexActivity.kt
@@ -0,0 +1,11 @@
+package fr.thibma.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokedexActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pokedex_activity)
+    }
+}
